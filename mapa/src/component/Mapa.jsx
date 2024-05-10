0dca81ab97d1c54c4604f9968fbd36034cefbc92
joashneves <joashneves@gmail.com>
2024-05-10 12:58:50 -0300

    testando ai, testei viu

--- mapa/src/component/Mapa.jsx
+++ mapa/src/component/Mapa.jsx
@@ -1,23 +1,71 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, Circle  } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
+// Importando o ícone de marcador personalizado
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Configurando o ícone personalizado
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const Mapa = () => {
-    
-    return <MapContainer center={[51.505, -0.09]} zoom={12} style={{height: '350px'}}>
-      <TileLayer
-        attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-            <span>
-              A pretty CSS3 popup. <br/> Easily customizable.
-            </span>
-        </Popup>
-      </Marker>
-    </MapContainer>;
 
+  const [userLocation, setUserLocation] = useState([-21.1058688, -41.0451968]);
+  
+
+  useEffect(() => {
+      // Obtendo a localização do usuário
+      if ("geolocation" in navigator) {
+          navigator.geolocation.getCurrentPosition(
+              (position) => {
+                  setUserLocation([position.coords.latitude, position.coords.longitude]);
+                  
+              },
+              (error) => {
+                  console.error('Erro ao obter localização do usuário:', error);
+              }
+          );
+      } else {
+          console.error('Geolocalização não suportada neste navegador.');
+      }
+      console.log(userLocation)
+  }, []); // A função useEffect só será executada uma vez, quando o componente for montado
+
+    const handleMapClick = (e) => {
+      console.log('Clicou')
+  };
+    return (
+        <MapContainer center={userLocation}
+         zoom={12}
+         style={{ height: '450px', width: '840px' }}
+       // Definindo o manipulador de eventos de clique no mapa
+        >
+            <TileLayer
+            
+                attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+             {/* Adicione um círculo ao mapa */}
+             <Circle
+                center={userLocation}
+                pathOptions={{
+                    color: 'red',
+                    fillColor: '#f03',
+                    fillOpacity: 0.5,
+                    radius: 500
+                }}
+            />
+        </MapContainer>
+    );
 }
 
 export default Mapa;
